4afe304da7d38634f122f754c1982b767da3dd58
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-06-10 01:52:46 +0200

    control flow and conditional statements

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,48 @@
+/** Conditionals */
+let userRole = 'admin';
+let accessLevel;
+
+if (userRole === 'admin') {
+  accessLevel = 'Full access granted';
+} else if (userRole === 'manager') {
+  accessLevel = 'Limited access granted';
+} else {
+  accessLevel = 'No access granted';
+}
+
+console.log('Access Level:', accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn) {
+  if (userRole === 'admin') {
+    userMessage = 'Welcome, Admin!';
+  } else {
+    userMessage = 'Welcome, User!';
+  }
+} else {
+  userMessage = 'Please log in to access the system.';
+}
+
+console.log('User Message:', userMessage);
+
+/** Switch statement */
+let userType = 'subscriber';
+let userCategory;
+
+switch (userType) {
+  case 'admin':
+    userCategory = 'Administrator';
+    break;
+  case 'manager':
+    userCategory = 'Manager';
+    break;
+  case 'subscriber':
+    userCategory = 'Subscriber';
+    break;
+  default:
+    userCategory = 'Unknown';
+}
+
+console.log('User Category:', userCategory);
