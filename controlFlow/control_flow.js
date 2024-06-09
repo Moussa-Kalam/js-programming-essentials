5f7480fa38b17a8c48a51f0a5cecf0470215de61
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-06-10 01:54:36 +0200

    Ternary operator

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -46,3 +46,12 @@ switch (userType) {
 }
 
 console.log('User Category:', userCategory);
+
+/** Ternary operator */
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated
+  ? 'Authenticated'
+  : 'Not authenticated';
+
+console.log('Authentication Status:', authenticationStatus);
