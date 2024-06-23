2b089ce321e097707191d45638732e1dd0f98fce
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-06-23 18:51:05 +0200

    feat; Implement clearing all the tasks

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -3,6 +3,7 @@ const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
 const taskList = document.getElementById('taskList');
 const clearCompletedBtn = document.getElementById('clearCompletedBtn');
+const clearAllBtn = document.getElementById('clearAllBtn');
 
 let tasks = [];
 
@@ -48,7 +49,14 @@ function clearCompletedTasks() {
   displayTasks();
 }
 
+// Clear all the tasks
+function clearAllTasks() {
+  tasks.length = 0;
+  displayTasks();
+}
+
 addTaskBtn.addEventListener('click', addTask);
 clearCompletedBtn.addEventListener('click', clearCompletedTasks);
+clearAllBtn.addEventListener('click', clearAllTasks);
 
 displayTasks();
